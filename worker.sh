e441ecefd34682d76dd90938a57b0129408c235f
Rodrigo Pronunciati <rodpro@hotmail.com>
2022-09-27 16:09:22 -0300

    Docker Cluster Swarm with Vagrant

--- worker.sh
+++ worker.sh
@@ -1,1 +1,1 @@
-    docker swarm join --token SWMTKN-1-3pj8k0i4tn77bd93a0yxhgh36hxuef5q5oyg1732rztnfy29ll-a94q0ipwgrjs4xikzyb4yb3n5 10.10.10.100:2377
+    docker swarm join --token SWMTKN-1-2mck0go3yd1z7efgojafgtlrp8s4vlueyr9w962b7l1o052zqw-1ozinoi1a1m9s9wv7961fsn0j 10.10.10.100:2377
